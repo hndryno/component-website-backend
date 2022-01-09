e001aef3468ccef48d7a2dbaa13858292aecc1a0
hendriyono <hendriyono97@gmail.com>
2022-01-09 16:43:53 +0700

    setting database config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,19 +1,19 @@
-// import {Sequelize} from 'sequelize'
-
-// let db_name = process.env.DB_NAME
-
-// const db = new Sequelize('db_component_website', 'root', 'Hendriyono1!', {
-//     host: '127.0.0.1',
-//     dialect: 'mysql',
-// })
-
-// export default db;
-
 import {Sequelize} from 'sequelize'
 
-const db = new Sequelize('db_component_website', 'root', '', {
+let db_name = process.env.DB_NAME
+
+const db = new Sequelize('db_component_website', 'root', 'Hendriyono1!', {
     host: '127.0.0.1',
     dialect: 'mysql',
 })
 
 export default db;
+
+// import {Sequelize} from 'sequelize'
+
+// const db = new Sequelize('db_component_website', 'root', '', {
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+// })
+
+// export default db;
