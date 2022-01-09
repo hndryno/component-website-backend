9cd8e1423f392634528a785a19bf5b51c654f97f
hendriyono <hendriyono97@gmail.com>
2022-01-09 10:37:34 +0700

    setup db production config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,6 +1,6 @@
 import {Sequelize} from 'sequelize'
 
-const db = new Sequelize('db_component_website', 'root', '', {
+const db = new Sequelize('db_component_website', 'root', 'Hendriyono1', {
     host: '127.0.0.1',
     dialect: 'mysql',
 })
